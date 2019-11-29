05aa86f535f036b614025cf940d0cf5423fe260b
POLYAKOV Lev <Lev.POLYAKOV@raiffeisen.ru>
2019-11-29 11:45:45 +0300

    init

---
+++ LDATools/Classes/LDASum.swift
@@ -0,0 +1,16 @@
+//
+//  LDASum.swift
+//  LDATools
+//
+//  Created by POLYAKOV Lev on 29/11/2019.
+//
+
+import Foundation
+
+public class LDASum {
+    
+    public func sum<T: Numeric>(a: T, b: T) -> T {
+        return a + b
+    }
+
+}
